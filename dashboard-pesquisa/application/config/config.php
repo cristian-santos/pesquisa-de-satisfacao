0a11160ce8e2965f9575cc488806c1ec64a1b363
cristian-santos <cristiansantosti@gmail.com>
2022-01-14 22:35:04 -0300

    ajuste da config

--- dashboard-pesquisa/application/config/config.php
+++ dashboard-pesquisa/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/meus-projetos/dashboard-pesquisa/';
+$config['base_url'] = 'http://localhost/meus_projetos/pesquisa-de-satisfacao/dashboard-pesquisa/';
 
 /*
 |--------------------------------------------------------------------------
